59bf9d4ef6d92492fcb1117d850adb4ada7b9bf7
Karlo Siladi <karlo.siladi@DPMDevOps.onmicrosoft.com>
2022-12-25 17:38:42 +0100

    Project initialized

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import InteractiveCommentSection from "./components/views/InteractiveCommentSection.vue";
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <InteractiveCommentSection />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/views/InteractiveCommentSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Interactive comment section</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InteractiveCommentSection",
+  // props: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
